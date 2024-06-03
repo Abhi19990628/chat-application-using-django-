a6993a3ec2185db4418aeb5786bfd4168cd35319
abhishek kumar <92795729+Abhi19990628@users.noreply.github.com>
2024-06-03 22:54:51 +0530

    Update models.py

--- chat/models.py
+++ chat/models.py
@@ -6,3 +6,13 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.content}'
